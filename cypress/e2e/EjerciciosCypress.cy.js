4876bbc8ad68d340de3b9179427f1323c7ba3639
unknown <YaochoaB2023>
2023-07-24 19:30:40 -0500

    realice un nuevo spcec para nuevos ejercicios

--- cypress/e2e/EjerciciosCypress.cy.js
+++ cypress/e2e/EjerciciosCypress.cy.js
@@ -54,11 +54,11 @@
 // })
 
 // 8 agrega mas comandos y aserciones
-// describe('test by yeison ochoa', () => {
-//   it('gets, types and asserts', () => {
-//     cy.visit('https://example.cypress.io')
-//     cy.contains('type').click()
-//     cy.url().should('include', '/commands/actions')
-//     cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com')  
-//   })
-// })
+describe('test by yeison ochoa', () => {
+  it('gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('type').click()
+    cy.url().should('include', '/commands/actions')
+    cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com')  
+  })
+})
---
+++ cypress/e2e/EjerciciosYeison.cy.js
@@ -0,0 +1,49 @@
+// ejercicio 9
+// describe('test by yeison ochoa', () => {
+//   it('passes', () => {
+//     cy.visit('https://www.mercadolibre.com.co/')
+//     cy.contains('Categorías')
+//   })
+// })
+
+// ejercicio cypress
+//9
+// describe('test by yeison ochoa', () => {
+//   it('deberia resolver la suma 10 + 20', () => {
+//     cy.log(10 + 20)
+//     cy.expect(10 + 20).to.equal(30)
+//   })
+// })
+
+//10
+// describe('test by yeison ochoa', () => {
+//   it('deberia resolver la resta 10 - 20', () => {
+//     cy.log(10 - 20)
+//     cy.expect(10 - 20).to.equal(-10)
+//   })
+// })
+
+//11
+// describe('test by yeison ochoa', () => {
+//   it('deberia resolver la division 10 / 20', () => {
+//     cy.log(10 / 20)
+//     cy.expect(10 / 20).to.equal(0.5)
+//   })
+// })
+
+//12
+// describe('test by yeison ochoa', () => {
+//   it('deberia resolver la multiplicacion 10 * 20', () => {
+//     cy.log(10 * 20)
+//     cy.expect(10 * 20).to.equal(200)
+//   })
+// })
+
+//12 uso de variables
+// describe('test by yeison ochoa', () => {
+//   it('deberia resolver la ecuacion x + 2 = 5', () => {
+//     let x = 3;
+//     cy.log(x + 2)
+//     cy.expect(x+2).to.equal(5)
+//   })
+// })
