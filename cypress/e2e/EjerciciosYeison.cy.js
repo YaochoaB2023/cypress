48cc210781eefb1b1defcb3fadeb4eda0341b420
yaochoaB2023 <ochoabautistayeisonalberto@gmail.com>
2023-07-31 07:40:04 -0500

    agregue operaciones con lista numerica corregida y los test aprovados

--- cypress/e2e/EjerciciosYeison.cy.js
+++ cypress/e2e/EjerciciosYeison.cy.js
@@ -39,7 +39,7 @@
 //   })
 // })
 
-//12 uso de variables
+//13 uso de variables
 // describe('test by yeison ochoa', () => {
 //   it('deberia resolver la ecuacion x + 2 = 5', () => {
 //     let x = 3;
@@ -47,3 +47,46 @@
 //     cy.expect(x+2).to.equal(5)
 //   })
 // })
+
+// 14 operaciones con lista numericas
+describe("test by yeison ochoa", () => {
+    it("realizar operacion con listas numericas", () => {
+        // lista de nuemeros
+        const listaNumeros = [10, 5, 8, 12, 4];
+
+        // calcular la suma de los numeros en la lista
+        let suma = 0;
+        for(let numero of listaNumeros) {
+            suma += numero;
+        }
+
+        // calcular el producto de los numero de la lista
+        let producto = 1;
+        for(let numero of listaNumeros) {
+            producto *= numero;
+        }
+
+        // calcular el promedio de los numero de la lista
+        let promedio = suma / listaNumeros.length;
+
+        // calcular el numero minimo y maximo de la lista de nuemros
+
+        let minimo = Math.min(...listaNumeros);
+        let maximo = Math.max(...listaNumeros);
+
+        // mostrar los resultados en la consola de cypress
+        cy.log(`lista de numeros: ${listaNumeros}`);
+        cy.log(`Suma: ${suma}`);
+        cy.log(`producto: ${producto}`);
+        cy.log(`promedio: ${promedio}`);
+        cy.log(`minimo: ${minimo}`);
+        cy.log(`maximo: ${maximo}`);
+
+        // verificar los resultados usando las acerciones de cypress
+        cy.wrap(suma).should('eq',39);
+        cy.wrap(producto).should('eq',19200);
+        cy.wrap(promedio).should('eq',7.8);
+        cy.wrap(minimo).should('eq',4);
+        cy.wrap(maximo).should('eq',12);
+    });
+});
